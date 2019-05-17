008a7be0acc318296dfdf930b602a4c0493f4ef1
Victor Feijo <victor.feijoa@gmail.com>
2019-05-17 18:10:54 -0300

    Removes proptypes from Icon

--- client/src/components/Icon.tsx
+++ client/src/components/Icon.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import * as React from 'react'
 import styled from 'styled-components';
 
-import logo from './icons/logo.png';
+import computer from './icons/computer.png';
 import horse from './icons/horse.png';
 import hourglass from './icons/hourglass.gif';
+import logo from './icons/logo.png';
 import program from './icons/program.png';
-import computer from './icons/computer.png';
 
 const StyledContainer = styled.span`
   display: inline-block;
@@ -29,22 +28,12 @@ export const Icon = ({ name, size, ...otherProps }) => {
   return (
     <StyledContainer
       style={{
-        width: size ? size : "30px",
-        height: size ? size : "30px"
+        width: size ? size : '30px',
+        height: size ? size : '30px'
       }}
       {...otherProps}
     >
       <StyledImg src={icons[name]} alt={name} />
     </StyledContainer>
   );
-};
-
-Icon.defaultProps = {
-  name: 'logo',
-  size: '30px',
-};
-
-Icon.propTypes = {
-  name: propTypes.string,
-  size: propTypes.oneOfType([propTypes.string, propTypes.number]),
 };
